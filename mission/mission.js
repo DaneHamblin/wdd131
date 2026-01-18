95f86c8178d88674d05e616317ab007d337d3d32
DaneHamblin <ham19031@byui.edu>
2026-01-17 23:53:28 -0700

    mission_complete

--- mission/mission.js
+++ mission/mission.js
@@ -3,10 +3,10 @@ const themeSelector = document.getElementById("theme");
 function changeTheme(e) {
   if (themeSelector.value == "dark") {
     document.body.classList.add("dark");
-    document.querySelector(".logo").src = "byui-logo_white.png";
+    document.querySelector(".logo").src = "byui-logo-white.png";
   } else {
     document.body.classList.remove("dark");
-    document.querySelector(".logo").src = "logo.webp";
+    document.querySelector(".logo").src = "byui-logo-blue.webp";
   }
 }
 
